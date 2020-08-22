801f258f40e7ecbacc0b677dd7081b5764125b25
eduardo0lima <55849785+eduardo0lima@users.noreply.github.com>
2020-08-22 15:33:59 -0300

    Add Department class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,55 +1,10 @@
 package application;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.PreparedStatement;
-import db.DB;
-import db.DbException;
-import db.DbIntegrityException;
-
 public class Program {
 
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) {
 	
 		
-		Connection conn = null;
-		Statement st = null;
-		
-		try {
-			
-			conn = DB.getConnection();
-			
-			st = conn.createStatement();
-			conn.setAutoCommit(false);
-			
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 "
-					+ "WHERE DepartmentId = 1");
-			/*
-			 * int x = 1; if(x<2) { throw new SQLException("Fake error!!!!!!!"); }
-			 */
-			
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 "
-					+ "WHERE DepartmentId = 2");
-			
-			System.out.println("Rows1 = " + rows1);
-			System.out.println("Rows2 = " + rows2);
-			
-			conn.commit();
-			
-		}catch (SQLException e){
-			try {
-				conn.rollback();
-				throw new DbException("Transaction rolled back caused by " + e.getMessage() + ".");
-			} catch (SQLException e1) {
-				// TODO Auto-generated catch block
-				throw new DbException("Error trying to roolback caused by " + e1.getMessage() + ".");
-			}
-		}
-		
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+	
 	}
 }
